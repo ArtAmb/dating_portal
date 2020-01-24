d8dbefdb379e4224262f5694916a591d51d71eda
ArtAmb <artur.ambrolewicz@gmail.com>
2020-01-25 00:03:23 +0100

    fix after merge

--- src/gui/angular/src/app/routes.ts
+++ src/gui/angular/src/app/routes.ts
@@ -9,7 +9,7 @@ export const APP_ROUTES = [
   { path: "", component: MainViewComponent },
   { path: "profil-edit", component: ProfilEditComponent },
   { path: "configuration", component: TagsConfigurationComponent },
-  { path: "profil-view/:id", component: ProfilViewComponent }
+  { path: "profil-view/:id", component: ProfilViewComponent },
   { path: "profil-view", component: ProfilViewComponent },
   { path: "matching-result", component: MatchingResultComponent }
 ];
